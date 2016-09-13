1c74241fc47594cee4a8267f37ec5feb098017b9
mgochoa <mochoa1127@gmail.com>
2016-09-13 10:01:11 -0500

    Cors

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GreetingController {
 
